7d9035eec87b3dc9b13eb1ddd17e2e93b8e677cd
Chaima <mac@chaima-2.local>
2025-05-07 23:07:52 +0100

    en cours de travai

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,13 +4,21 @@ import Sidebar from './Sidebar';
 
 function Layout() {
   return (
-    <>
-      <Navbar />
-      <Sidebar/>
-      <main className="p-4">
-        <Outlet />
-      </main>
-    </>
+    <div className="flex h-screen">
+      {/* Sidebar à gauche */}
+      <Sidebar />
+
+      {/* Contenu principal à droite */}
+      <div className="flex flex-col flex-1">
+        {/* Navbar en haut */}
+        <Navbar />
+
+        {/* Zone principale */}
+        <main className="p-4 flex-1 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 }
 
