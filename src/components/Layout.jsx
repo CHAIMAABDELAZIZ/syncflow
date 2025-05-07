c8f39347714bf97a07887d1aa62d0a340ba60959
Chaima <mac@chaima-2.local>
2025-05-07 23:00:48 +0100

    WIP: en cours de travail

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 function Layout() {
   return (
     <>
       <Navbar />
+      <Sidebar/>
       <main className="p-4">
         <Outlet />
       </main>
